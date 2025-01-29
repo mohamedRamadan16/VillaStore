cbb65d7bf6f75da1052421b5641cef1e6ad09d12
mohamedRamadan16 <113842111+mohamedRamadan16@users.noreply.github.com>
2025-01-29 15:16:13 +0200

    Add project files.

---
+++ MagicVilla/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVilla.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VillaAPIController:ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetAllVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa() {Id = 1, Name = "RRVS19"},
+                new Villa() {Id = 2, Name = "RRVS18"},
+                new Villa() {Id = 3, Name = "RRVS17"}
+            };
+        }
+    }
+}
